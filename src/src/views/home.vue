90b7a98aaafe2af38baf4014261e7e4aa560de08
HummingTrippy <aqua401@icloud.com>
2019-08-29 00:44:26 +0900

    カートのUI作成

---
+++ src/src/views/home.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <div class="home">      
+
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'home',
+        data: function () {
+            return {
+                TaskName: '',
+                TaskDedLine: '',
+                TaskUsingTime: '',
+                TaskPlace: '',
+                TaskFeeling: '',
+                isShow: false,
+            };
+        },
+    }
+</script>
+
+<style scoped>
+</style>
